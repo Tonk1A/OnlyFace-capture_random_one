e6b637ac946e6bb80de8934fdeb675cf35bf7989
Tonk1A <tonklamonthong6@gmail.com>
2024-08-11 13:36:25 +0700

    wip v0.3

--- OnlyFace.py
+++ OnlyFace.py
@@ -16,14 +16,11 @@ face_id = 0
 face_dict = {}
 
 def delete_all_files(directory):
-    """Delete all files and subdirectories in the given directory."""
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
 
