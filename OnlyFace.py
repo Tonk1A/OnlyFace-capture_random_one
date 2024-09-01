b15647ccf909e5ab8e303fd9f0ead41e15df5c32
ViperDew <ocs.viper01@gmail.com>
2024-09-01 16:48:24 +0700

    ืีืีnu

--- OnlyFace.py
+++ OnlyFace.py
@@ -1,8 +1,9 @@
 import sys
-import cv2
 import os
 import random
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QMessageBox, QDialog
+import cv2
+from PyQt5.QtWidgets import QLabel, QPushButton, QVBoxLayout, QWidget, QMessageBox, QDialog  # pylint: disable=E0611
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer, Qt
 
@@ -10,14 +11,17 @@ from PyQt5.QtCore import QTimer, Qt
 video_cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-saved_img = "images"
+saved_img = "images" # pylint: disable=C0103
 if not os.path.exists(saved_img):
     os.mkdir(saved_img)
 
-face_id = 0
+face_id = 0 # pylint: disable=C0103
 
 
-def delete_all_files(directory):
+def delete_all_files(directory): # pylint: disable=C0303
+    '''
+    hggvfhg
+    '''
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         try:
@@ -25,47 +29,42 @@ def delete_all_files(directory):
                 os.unlink(file_path)
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
-
-
 class MyApp(QWidget):
+    '''
+    wrrw
+    '''
     def __init__(self):
         super().__init__()
-
         self.initUI()
-
-
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(30)  # อัปเดตเฟรมทุก ๆ 30 ms
-
     def initUI(self):
-       
+        '''
+        werwr
+        '''
         layout = QVBoxLayout()
 
-      
         self.video_label = QLabel("Camera Feed", self)
         layout.addWidget(self.video_label)
 
-       
         self.btn_random = QPushButton('Random (r)', self)
         self.btn_random.clicked.connect(self.random_image)
         layout.addWidget(self.btn_random)
 
-       
         self.btn_delete = QPushButton('Delete (d)', self)
         self.btn_delete.clicked.connect(self.delete_images)
         layout.addWidget(self.btn_delete)
 
-       
         self.setLayout(layout)
-
-        
         self.setWindowTitle('Face Detection App')
         self.setGeometry(300, 300, 600, 500)
         self.show()
 
     def keyPressEvent(self, event):
-        
+        '''
+        dweed
+        '''
         if event.key() == Qt.Key_R:
             self.random_image()
         elif event.key() == Qt.Key_D:
@@ -74,56 +73,51 @@ class MyApp(QWidget):
             self.close()
 
     def update_frame(self):
+        '''
+        wef
+        '''
         ret, frame = video_cap.read()
         if ret:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-            
             face_count = len(faces)
             for (x, y, w, h) in faces:
-                frame = cv2.rectangle(frame, (x, y), 
-                (x + w, y + h), (0, 255, 0), 2)
+                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 cv2.putText(frame, f'Faces: {face_count}', (x, y - 10),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-            
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
             self.video_label.setPixmap(QPixmap.fromImage(image))
 
     def random_image(self):
+        '''
+        werw
+        '''
         ret, frame = video_cap.read()
         if not ret:
             print("Failed to capture image")
             return
-        
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-        
         if len(faces) > 0:
             global face_id
             face_id += 1
-            
-            
             (x, y, w, h) = random.choice(faces)
             img_name = os.path.join(saved_img, f"face_{face_id}.jpg")
-
-           
             face_img = frame[y:y + h, x:x + w]
             cv2.imwrite(img_name, face_img)
-
-            
             self.show_captured_face(img_name)
             print(f"Image saved as {img_name}")
         else:
             self.video_label.setText("No Face Detected")
-
     def show_captured_face(self, img_name):
+        '''
+        wrewe
+        '''
+        img_name = os.path.join(saved_img, f"face_{face_id}.jpg")
         dialog = QDialog(self)
         dialog.setWindowTitle("Captured Face")
-        
         layout = QVBoxLayout()
-
-        
         pixmap = QPixmap(img_name)
         label = QLabel()
         label.setPixmap(pixmap)
@@ -133,10 +127,12 @@ class MyApp(QWidget):
         dialog.exec_()
 
     def delete_images(self):
+        '''
+        dwfe
+        '''
         reply = QMessageBox.question(self, 'Delete Confirmation', 
-                                     'Are you sure you want to delete all images?', 
+'Are you sure you want to delete all images?', 
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-
         if reply == QMessageBox.Yes:
             delete_all_files(saved_img)
             self.video_label.setText('No Image')
@@ -145,6 +141,9 @@ class MyApp(QWidget):
             pass
 
     def closeEvent(self, event):
+        '''
+        ewf
+        '''
         video_cap.release()
         event.accept()
 
